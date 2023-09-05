8c59f32e591425fb73f6460238de1d08285ec52c
Andriy Plyska <andriyplyska@MBP-Andriy.home>
2023-09-05 17:52:16 +0200

    add cars section

---
+++ utils/index.ts
@@ -0,0 +1,38 @@
+export async function fetchCars() {
+  const url = "https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla";
+  const options = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "ce1fccc5e8msh53aa4e40b54e96ap1fb820jsn62a0209f4119",
+      "X-RapidAPI-Host": "cars-by-api-ninjas.p.rapidapi.com",
+    },
+  };
+
+  const response = await fetch(url, options);
+
+  const result = await response.json();
+
+  return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+  const basePricePerDay = 50; // Base rental price per day in dollars
+  const mileageFactor = 0.1; // Additional rate per mile driven
+  const ageFactor = 0.05; // Additional rate per year of vehicle age
+
+  // Calculate additional rate based on mileage and age
+  const mileageRate = city_mpg * mileageFactor;
+  const ageRate = (new Date().getFullYear() - year) * ageFactor;
+
+  // Calculate total rental rate per day
+  const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+
+  return rentalRatePerDay.toFixed(0);
+};
+
+export const mpgToEuropeanStandard = (mpg: number) => {
+  const conversionFactor = 235.214;
+  const litersPer100Km = conversionFactor / mpg;
+
+  return litersPer100Km.toFixed(1);
+};
